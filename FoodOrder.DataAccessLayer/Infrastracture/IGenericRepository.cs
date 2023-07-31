073d3964d6889acb8838d6a89a6bca7de443f204
bhagchandmangi <bhagchandmangi@gmail.com>
2023-07-31 19:08:59 +0500

    Create Category interface and Model

---
+++ FoodOrder.DataAccessLayer/Infrastracture/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.DataAccessLayer.Infrastracture
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Insert(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+    }
+}
