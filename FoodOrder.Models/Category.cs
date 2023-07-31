635449a9622762f61ba2438b5e9794662ccbb918
bhagchandmangi <bhagchandmangi@gmail.com>
2023-07-31 19:00:05 +0500

    Create Multiple Projects within main Project

---
+++ FoodOrder.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Models
+{
+    public class Category
+    {
+        int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+    }
+}
