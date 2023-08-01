955523ef568466dadd8f0a02d6b85f4e8659fb34
bhagchandmangi <bhagchandmangi@gmail.com>
2023-08-01 20:04:45 +0500

    Interface and their implementation is created

--- FoodOrder.DataAccessLayer/ApplicationDbContext.cs
+++ FoodOrder.DataAccessLayer/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodOrder.DataAccessLayer
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+            { 
+        }
     }
 }
