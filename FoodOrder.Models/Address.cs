f0897f32534caa7f0ba8d154969f0a458196e624
bhagchandmangi <bhagchandmangi@gmail.com>
2023-08-04 11:27:44 +0500

    Create Models order, orderitem, cart, cartitem, and Address

---
+++ FoodOrder.Models/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Models
+{
+    public class Address
+    {
+        public Address(string street, string locolity, string city, string zipcode)
+        {
+            Street = street;
+            Locolity = locolity;
+            City = city;
+            Zipcode = zipcode;
+        }
+
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Locolity { get; set;}
+        public string City { get; set;}
+        public string Zipcode { get; set;}
+        public string PhoneNumber { get; set;}
+        public int UserId { get; set;}
+    }
+}
---
+++ FoodOrder.Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public int UserId { get; set; } 
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+    }
+}
