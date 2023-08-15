067bb9cf6c9477df13e95b0a9660ab6db5b59b57
bhagchandmangi <bhagchandmangi@gmail.com>
2023-08-15 17:46:17 +0500

    Complete Application DbContext Class

--- FoodOrder.DataAccessLayer/ApplicationDbContext.cs
+++ FoodOrder.DataAccessLayer/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FoodOrder.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,8 +11,19 @@ using System.Threading.Tasks;
 
 namespace FoodOrder.DataAccessLayer
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : 
+        IdentityDbContext<User, Role, int>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<PaymentDetails> PaymentDetais { get; set; }
     }
+    
 }
